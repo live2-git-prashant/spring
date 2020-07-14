14857b2f84c13b187d56820f8cc367f5bfe68021
live-git-prashant <cprashant80@gmail.com>
2020-07-13 20:35:59 -0500

    update spring

---
+++ ld1a-enterprise-app-bootstrap/src/main/java/com/live/ld1a_enterprise_app_bootstrap/App.java
@@ -0,0 +1,13 @@
+package com.live.ld1a_enterprise_app_bootstrap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
